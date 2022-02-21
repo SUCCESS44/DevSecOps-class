ebf3fd6b1df995325c61335b3a8403123c071141
SUCCESS44 <ononuju.okere@yahoo.com>
2022-02-21 05:30:34 -0500

    keyword.py modified

--- keyword.py
+++ keyword.py
@@ -4,7 +4,7 @@ These keywords are always available—you’ll never have to import them into yo
 
 Examples of python keywords
 
-1.def keyword
+def keyword
 Python def keyword is used to define a function, it is placed before a function name 
 that is provided by the user to create a user-defined function
 
@@ -13,7 +13,7 @@ def my_function():
 
 my_function()
 
-2.return keyword
+return keyword
 The return keyword in Python exits a function and tells Python to run the rest of
 the main program. A return keyword can send a value back to the main program. 
 While values may have been defined in a function, you can send them back to your 
@@ -24,7 +24,7 @@ def myfunction():
 
 print(myfunction())
 
-3. class keyword
+class keyword
 The class keyword is used to create a class. A class is like an object constructor. 
 See the example below to see how we can use it to create an object.
 
