4f98c023d7ecacb5f2de0d3e2c6145ed0b238772
SUCCESS44 <ononuju.okere@yahoo.com>
2022-02-22 07:32:54 -0500

    dailies_21stFeb.py added

---
+++ dailies_21stFeb.py
@@ -0,0 +1,24 @@
+#!/home/ojokere/Development/DevSecOps/bin/python
+def calculate (num1, num2=4):
+    res = num1 * num2
+    print(res)
+
+    calculate(5, 6)
+
+#  What is the output of the following code?   
+var = "James" * 2 * 3
+print(var)
+
+#what is the output of the following code
+x = 36/4 * (3 + 2) * 4 + 2
+print(x)
+
+#what is the output of the following code
+var1 = 1
+var2 = 2
+var3 = 3
+print(var1 + var2 + var3)
+
+#what is the output of the following code?
+p, q, r = 10, 20, 30
+print(p, q, r)
