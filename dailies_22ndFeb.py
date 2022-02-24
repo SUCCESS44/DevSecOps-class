3c3f095c4f7a1521b37787425bb14dad00c91c25
SUCCESS44 <ononuju.okere@yahoo.com>
2022-02-24 18:37:14 -0500

    dailes_22ndFeb.py added

---
+++ dailies_22ndFeb.py
@@ -0,0 +1,16 @@
+#what is the maximum length of a python idntifier?
+79 charaters
+#which of the following is an invalid statement
+x y z = 1 2 3
+#Which of the following are valid Python variable names:
+ver1.3
+return
+home_address
+route66
+Age
+#All keywords in Python are in?
+Lower case
+#Which of the following are Python reserved words (keywords):
+and
+class
+None
